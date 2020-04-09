88a987036cc548a4bc28ccedbb017245da9188f9
ByteWoofer <chris@teamofdefenders.com>
2020-04-09 15:26:26 -0500

    Trimmed random locations for dc26/randtick.py to only visible pixels

--- dc26/randtick.py
+++ dc26/randtick.py
@@ -14,9 +14,17 @@ class randtick:
 		if(self.count<self.reset):
 			self.count+=1
 			row = random.randint(0,6)
-			col = random.randint(0,17)
-			value = random.randint(0,255)
+			if row == 0:
+				col = random.randint(1,16)
+			elif row == 5:
+				col = random.choice(list(range(0,8))+list(range(11,18)))
+			elif row == 6:
+				col = random.choice(list(range(1,7))+list(range(12,17)))
+			else:
+				col = random.randint(0,18)
+			value = random.randint(50,255)
 			dcfurs.set_pixel(col,row,value)
+			# print(col," ",row)
 		else:
 			self.count=0
 			dcfurs.clear()
